bdb5aa79cff7f0047c886ad24cc638bc9cb8eb37
graham mather <gmather@gmail.com>
2014-09-30 12:13:56 -0600

    use ojdbc driver as a module

--- scripts/atg/provision_install_crs_artifacts.sh
+++ scripts/atg/provision_install_crs_artifacts.sh
@@ -7,6 +7,10 @@ cp /vagrant/scripts/atg/crs_artifacts/ATGProduction.xml /vagrant/scripts/atg/crs
 /home/vagrant/ATG/ATG11.1/home/bin/runAssembler -server "ATGProduction" -jboss "/home/vagrant/jboss/standalone/deployments/ATGProduction/ATGProduction.ear" -m Store.EStore DafEar.Admin DPS DSS ContentMgmt DCS.PublishingAgent DCS.AbandonedOrderServices ContentMgmt.Endeca.Index DCS.Endeca.Index Store.Endeca.Index DAF.Endeca.Assembler PublishingAgent DCS.Endeca.Index.SKUIndexing Store.Storefront Store.Recommendations DCS.ClickToConnect Store.Fluoroscope Store.Fulfillment Store.KnowledgeBase
 /home/vagrant/ATG/ATG11.1/home/bin/runAssembler -server "ATGPublishing" -jboss "/home/vagrant/jboss/standalone/deployments/ATGPublishing/ATGPublishing.ear" -m DCS-UI.Versioned BIZUI PubPortlet DafEar.Admin ContentMgmt.Versioned DCS-UI.SiteAdmin.Versioned SiteAdmin.Versioned DCS.Versioned DCS-UI Store.EStore.Versioned Store.Storefront ContentMgmt.Endeca.Index.Versioned DCS.Endeca.Index.Versioned Store.Endeca.Index.Versioned DCS.Endeca.Index.SKUIndexing Store.KnowledgeBase 
 
+# make sure jboss knows to deploy
+touch /home/vagrant/jboss/standalone/deployments/ATGProduction/ATGProduction.ear.dodeploy
+touch /home/vagrant/jboss/standalone/deployments/ATGPublishing/ATGPublishing.ear.dodeploy
+
 # copy ATG server configs
 unzip -n /vagrant/scripts/atg/crs_artifacts/server-ATGProduction.zip -d /home/vagrant/ATG/ATG11.1/home/servers
 unzip -n /vagrant/scripts/atg/crs_artifacts/server-ATGPublishing.zip -d /home/vagrant/ATG/ATG11.1/home/servers
